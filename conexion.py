d8e6af7994467efd12488c45520599aafd0a3988
Richard Yancce <richard.yancce.tineo@gmail.com>
2025-03-27 12:14:55 -0500

    primer comentario

---
+++ conexion.py
@@ -0,0 +1,15 @@
+import pyodbc
+import streamlit as st
+
+# Función para inicializar la conexión
+def init_connection():
+    return pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + st.secrets["server"]
+        + ";DATABASE="
+        + st.secrets["database"]
+        + ";UID="
+        + st.secrets["username"]
+        + ";PWD="
+        + st.secrets["password"]
+    )
